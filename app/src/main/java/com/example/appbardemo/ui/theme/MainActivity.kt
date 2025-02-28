30782d0d26e3a0d66ed6e17731717397716ad7bd
Yasvi Vaghasiya <yasvivaghasiya119@gmail.com>
2025-02-28 18:11:34 +0530

    Grid

--- app/src/main/java/com/example/appbardemo/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/appbardemo/ui/theme/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.appbardemo.ui.theme
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appbardemo.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fabLayout : FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fabLayout = findViewById(R.id.fabContainer)
+        fabLayout.setOnClickListener {
+            showMenu()
+        }
+    }
+
+    private fun showMenu(){
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+        bottomSheetDialog.show()
     }
 }
