71f087cf038905937c98ab8f9404d16940ac0f53
Yasvi Vaghasiya <yasvivaghasiya119@gmail.com>
2025-02-28 15:50:23 +0530

    Grid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.google.android.material:material:1.10.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
